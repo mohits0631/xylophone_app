a27615fb3f0d1c5dccbb5e0ec46705fb95e9aad8
mohit singh <mohit.s0631@gmail.com>
2020-08-25 17:16:41 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade800,
+        title: Center(
+          child: Text('Ask Me Anything'),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print(ballNumber);
+          });
+        },
+        child: Center(
+          child: Image.asset(
+            'images/ball$ballNumber.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
